d64791264b19c6dfa84c5f9001150bde5b9d3c41
TRANNHUHAI <tranhaiexp@gmail.com>
2020-05-04 23:51:41 +0700

    get list

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const fs = require('fs');
 
 app.listen(port, () => console.log(`Server starting at ${port}`));
 app.set('view engine', 'pug')
+app.set('views', './views');
 
-const data = JSON.parse(fs.readFileSync('./data.json', {encoding: 'utf8'}));
+const data = fs.readFileSync('./data.json', {
+  encoding: 'utf8'
+});
+const lists = JSON.parse(data);
 
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+app.get('/list', function (req, res) {
+  res.render('list/index', {
+    lists
+  });
+});
