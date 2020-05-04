f0380adf4ca0a281bc475519054d59f470a6bf86
TRANNHUHAI <tranhaiexp@gmail.com>
2020-05-04 23:00:36 +0700

    create template engie pug

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const fs = require('fs');
+
 
 app.listen(port, () => console.log(`Server starting at ${port}`));
+app.set('view engine', 'pug')
+
+const data = JSON.parse(fs.readFileSync('./data.json', {encoding: 'utf8'}));
+
