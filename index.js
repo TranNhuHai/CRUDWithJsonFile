d37b5ec436e26c03bc0758210aec97fde278c888
TRANNHUHAI <tranhaiexp@gmail.com>
2020-05-05 07:17:36 +0700

    add delete

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ app.listen(port, () => console.log(`Server starting at ${port}`));
 app.set('view engine', 'pug')
 app.set('views', './views');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-let lists ;
-function loadData(){
+let lists;
+
+function loadData() {
   const data = fs.readFileSync('./data.json', {
     encoding: 'utf8'
   });
@@ -36,7 +39,7 @@ app.get('/list', function (req, res) {
 app.get('/list/add', function (req, res) {
   res.render('list/add');
 });
-app.post('/list/add', function(req, res) {
+app.post('/list/add', function (req, res) {
   loadData();
   const list = {
     id: lists[lists.length - 1].id + 1,
@@ -55,10 +58,21 @@ app.get('/list/update', function (req, res) {
 
 app.post('/list/update', function (req, res) {
   loadData()
-  const index = lists.findIndex((list) => list.name == req.body.old_name);
-  if(index >= 0) {
+  const index = lists.findIndex(list => list.name == req.body.old_name);
+  if (index >= 0) {
     lists[index].name = req.body.new_name;
   }
   saveList();
   res.redirect('/list');
 });
+app.get('/list/remove', function (req, res) {
+  loadData();
+  res.render('/list/remove');
+});
+app.delete('/list/remove', function (req, res) {
+  loadData();
+  const index = lists.findIndex(list => list.name == req.body.name);
+  lists.splice(index, 1);
+  saveList();
+  res.redirect('/list');
+})
